8a2696ac90a06309caada6ee2620828ab84e587e
K9Codez <97529994+K9Codez@users.noreply.github.com>
2022-07-19 18:56:31 +0100

    Added Files
    New CRDEV

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "F1"
+description "discord.gg/armedrp"
+version "2.0"
+
+files {
+    'index.html',
+    'audio/*.mp3',
+    'css/styles.css',
+    'video/*.mp4',
+    'img/*',
+    'img/*/*',
+    'js/main.js',
+    'video/video.mp4',
+}
+
+loadscreen 'index.html'
